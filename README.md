a624a576ed32059e29d6b81f433d6a9a7c9292e8
DeltaNedas <deltanedas@gmail.com>
2020-05-05 19:05:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 7 Seas
+
+Calls out pirates when joining a server.
+
+## Explicit content
+It use bad word
---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+// Prevent event handling multiple times
+if (!this.global.sevenSeas) {
+	this.global.sevenSeas = true;
+
+	const pirates = /CODEX|IGGGAMES/;
+
+	const yarr = player => {
+		print("player " + player)
+		if (player.name.match(pirates)) {
+			Call.sendChatMessage("actual retard pirating a free game");
+		}
+	};
+
+	Events.on(EventType.WorldLoadEvent, run(e => {
+		// Players are added shortly after loading
+		Time.run(60, run(() => {
+			const players = Vars.playerGroup.all();
+			players.each(cons(yarr));
+		}));
+	}));
+	Events.on(EcentType.PlayerJoinEvent, run(e => {
+		print("Event " + e)
+		yarr(e.player);
+	}));
+}
